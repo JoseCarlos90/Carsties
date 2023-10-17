5132e89fb1032896e050f3f73151c4302fa052e6
Jose carlos <jcgonzalez@lz2.es>
2023-10-17 21:09:25 +0200

    Agregamos poyecto de unit tests y integracion de tests

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -51,6 +51,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 });
 
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -69,3 +71,5 @@ catch (System.Exception e)
 }
 
 app.Run();
+
+public partial class Program {}
